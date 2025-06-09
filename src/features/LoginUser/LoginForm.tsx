ba1127f619ce55f7422e30da7f5d2acf0b632a6f
Matteo Conci <matteo.conci54@hotmail.it>
2025-06-09 19:34:55 +0200

    Login Form completed

--- src/features/LoginUser/LoginForm.tsx
+++ src/features/LoginUser/LoginForm.tsx
@@ -1,11 +1,23 @@
 import { useState } from "react";
+/*import { useSelector } from "react-redux";*/
 
-const LoginForm = () => {
+export type LoginFormProps = { 
+    onSubmit: (email: string, password: string) => void;
+};
+
+const LoginForm = ({ onSubmit }: LoginFormProps) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    /*const isLoading = useSelector*/
 
     return (
-        <form className="absolute w-[400px] h-[234px] left-[50px] top-[48px]">
+        <form 
+            onSubmit={e => {
+                e.preventDefault(); 
+                onSubmit(email, password);
+            }}
+            className="absolute w-[400px] h-[234px] left-[50px] top-[48px]"
+        >
             <div className="absolute w-[400px] h-[64px] left-0 top-0">
                 <label htmlFor="login-email">
                     Email/Phone Number
@@ -36,11 +48,15 @@ const LoginForm = () => {
                     className="box-border absolute w-[400px] h-[40px] left-0 top-[30px] border border-[rgba(0,0,0,0.3)] rounded-[8px]"
                 />
             </div>
+            <p className='absolute w-[119px] h-[17px] left-0 top-[177px] Inter not-italic font-normal text-sm text-purple-800 hover:underline hover:cursor-pointer'>Forget Password?</p>
+            <button
+                type="submit"
+                /*disabled={isLoading || !email || !password}*/
+                className="absolute w-[80px] h-[30px] left-0 top-[210px] bg-black rounded-[8px] text-white Inter not-italic font-normal text-sm leading-4 hover:cursor-pointer"
+            >Login</button>
+            <p className="absolute h-[17px] left-[150px] top-[215px] Inter not-italic font-normal text-sm text-purple-800 hover:underline hover:cursor-pointer">New to Shopp, Sign Up now!</p>
         </form>
     );
 };
 
 export default LoginForm;
-
-
-
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,12 @@
 import Logo from '../assets/Logo.svg';
 import LoginForm from '../features/LoginUser/LoginForm';
+import Footer from '../components/Footer';
 
 const LoginPage = () => {
+
+    const handleSubmit = () => {
+
+    };
 
     return (
         <div>
@@ -10,20 +15,19 @@ const LoginPage = () => {
             </header>
             <main>
                 <div className='absolute w-[500px] h-[350px] left-[470px] top-[139px] bg-[#FFFFFF] border-t border-t-[rgba(0,0,0,0.1)] shadow-[0_4px_4px_rgba(0,0,0,0.25)] rounded-[12px]'>
-                    <section>
-                        <LoginForm />
-                        <p className='absolute w-[119px] h-[17px] left-[50px] top-[225px] Inter not-italic font-normal text-sm leading-4 text-purple-800'>Forget Password?</p>
-                    </section>
+                    <LoginForm 
+                        onSubmit={handleSubmit}
+                    />
                 </div>
             </main>
-            <p>prova</p>
+            <footer>
+                <Footer />
+            </footer>
         </div>
     );
 };
 
 export default LoginPage;
-
-/* Forget Password? */
 
 
 
