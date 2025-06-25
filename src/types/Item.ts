8d86abcb40dd53abfb36891b9915800010ad8ee5
Nghiem Gia Bao <bao15022016@gmail.com>
2025-06-25 17:56:34 +0700

    Finish UI for suggestion of the day

--- src/types/Item.ts
+++ src/types/Item.ts
@@ -1,3 +1,5 @@
+//! All the type in here may not be accurate, I just create to match the display of the UI
+
 export interface FlashSaleItem {
     name: string;
     price: number;
@@ -5,4 +7,15 @@ export interface FlashSaleItem {
     discount: number;
     sold: number;
     isMall: boolean;
+}
+
+export interface Item {
+    name: string;
+    price: number;
+    image_url: string;
+    discount: number;
+    sold: number;
+    isMall: boolean;
+    isFreeShipping: boolean;
+    isBestPrice: boolean;
 }
